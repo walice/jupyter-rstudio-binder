23157d8a0f255d8159d700034777b2b97f79199c
Alice Lepissier <alice.lepissier@gmail.com>
2022-08-23 01:58:58 +0000

    Jupyter but no RStudio

---
+++ requirements.R
@@ -0,0 +1,13 @@
+# IllicitAI
+# Alice Lepissier
+# alice.lepissier@gmail.com
+
+## ## ## ## ## ## ## ## ## ## ##
+# REQUIREMENTS              ####
+## ## ## ## ## ## ## ## ## ## ##
+
+Sys.setenv(LIBARROW_BINARY = TRUE)
+install.packages("arrow", 
+                 repos = "http://cran.us.r-project.org",
+                 type = "source")
+install.packages(c("wesanderson", "sysfonts", "showtext", "GGally", "mapproj", "gridExtra"))
